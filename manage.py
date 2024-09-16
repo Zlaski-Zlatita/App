76aeed8a17cb7c031b995fb52f68aa4ccb219a83
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2024-09-16 17:40:53 +0500

    manage.py

--- manage.py
+++ manage.py
@@ -3,4 +3,20 @@
 import ...
 
 def main():
-  os.environ.setdefault('DJANNGO_')
+  os.environ.setdefault('DJANNGO_SETTINGS_MODULE', 'taskmanager.settings')
+  try:
+    from django.core.management import execute_from_command_line
+  except ImportError as exc:
+    raise ImportError(
+      "Couldn't import Dgango. Are you sure it's installed and "
+      "available on your PYTHONPATH environment variable? Did you "
+      "forget to activate a virtualisierung environment?"
+    ) from exc  
+execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+  main()
+  
+
+
+
